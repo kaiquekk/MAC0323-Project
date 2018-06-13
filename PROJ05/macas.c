42fc90dcee7f3ab44373267fd3c9512a47e2d2e8
yurickyh <yurickyussuke@hotmail.com>
2018-06-13 19:54:48 -0300

    Adding no such file error.
    Signed-off-by: yurickyh <yurickyussuke@hotmail.com>

--- PROJ05/macas.c
+++ PROJ05/macas.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 int main(int argc, char **argv){
+    const char *posDot = strchr(argv[1], 46);
     if(argc > 2){
         /*TO DOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO*/
         printf("ERROR: too many arguments\n");
     }
-    else{
-        const char *posDot = strchr(argv[1], 46);
-        if(strcmp(posDot, ".as") != 0){
-            /*TO DOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO*/
-            printf("ERROR: wrong file extension.\n");
-        }
-        else{
-            /*CALL assemble*/
-        }
+    else if(strcmp(posDot, ".as") != 0){
+        /*TO DOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO*/
+        printf("ERROR: wrong file extension.\n");
     }
+    else if(access(argv[1], R_OK) == -1){
+        /*TO DOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO*/
+        printf("ERROR: no such file.\n");
+    }
+    else{
+        /*CALL assemble*/
+    }        
 }
