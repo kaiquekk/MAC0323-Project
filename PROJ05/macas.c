cdb4573cf916511de96d69de2b452cac6c85075d
yurickyh <yurickyussuke@hotmail.com>
2018-06-13 19:45:26 -0300

    Setting some error messages when using assembler in a wrong way.
    Signed-off-by: yurickyh <yurickyussuke@hotmail.com>

--- PROJ05/macas.c
+++ PROJ05/macas.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-    
+#include <string.h>
+int main(int argc, char **argv){
+    if(argc > 2){
+        /*TO DOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO*/
+        printf("ERROR: too many arguments\n");
+    }
+    else{
+        const char *posDot = strchr(argv[1], 46);
+        if(strcmp(posDot, ".as") != 0){
+            /*TO DOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO*/
+            printf("ERROR: wrong file extension.\n");
+        }
+        else{
+            /*CALL assemble*/
+        }
+    }
 }
